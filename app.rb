47e1fb49396ee0be13b0bbfa9ef73dd39e4c7d3a
Guillermo Veneranda <guilleveneyou@gmail.com>
2015-10-08 17:06:43 -0200

    Mensaje Final Perdiste o Ganaste

--- app.rb
+++ app.rb
@@ -11,9 +11,19 @@ end
 
 post '/tomar' do
 	if (@@palabra.include? params['letra']) == false
-		@mensaje = "LETRA NO ES COINCIDENTE"
+		@@contador+=1	
+		if @@contador < 6 		
+			@mensaje = "LETRA NO ES COINCIDENTE"
+		else
+			@mensaje = "PERDISTE"
+			@@guiones = "_ _ _ _ _"
+			@@contador = 0
+		end
 	else
 		@@guiones = search(params['letra'])
+		if @@guiones == "C I R C O "
+			@mensaje = "PALABRA ADIVINADA"
+		end
 	end
 	erb :index
 end
